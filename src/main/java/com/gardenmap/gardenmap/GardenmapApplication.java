5f69f4d0caa188b47fb44363bf3dc5536d3f1424
Sol Turipe <solcis.bio@gmail.com>
2022-03-02 12:43:04 +0100

    first commit

---
+++ src/main/java/com/gardenmap/gardenmap/GardenmapApplication.java
@@ -0,0 +1,13 @@
+package com.gardenmap.gardenmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gardenmap/gardenmap/GardenmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gardenmap.gardenmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
