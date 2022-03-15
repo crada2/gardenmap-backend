91bbbcd30cbdb6f4127fa3442bf0b43d5e81a984
MayuRuru <aruizrull@protonmail.com>
2022-03-15 15:12:24 +0100

    add test

--- src/main/java/com/gardenmap/gardenmap/service/ProductServiceImpl.java
+++ src/main/java/com/gardenmap/gardenmap/service/ProductServiceImpl.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 
 public class ProductServiceImpl implements ProductService {
 
-    @Autowired
+
     private ProductRepository productRepository;
     private OwnerRepository ownerRepository;
 
