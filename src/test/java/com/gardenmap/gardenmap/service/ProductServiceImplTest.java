bb51dd4fd28e951488cadecad84b601e43c26f60
Sol Turipe <solcis.bio@gmail.com>
2022-03-17 12:06:42 +0100

    product service get by id test

--- src/test/java/com/gardenmap/gardenmap/service/ProductServiceImplTest.java
+++ src/test/java/com/gardenmap/gardenmap/service/ProductServiceImplTest.java
@@ -9,11 +9,14 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ProductServiceImplTest {
@@ -26,8 +29,9 @@ class ProductServiceImplTest {
     ProductRepository productRepository;
     @Mock
     OwnerRepository ownerRepository;
+
     @Test
-    void create() {
+    void ProductServiceCanCreateAProduct() {
         Product product = new Product();
         var newOwner = new Owner();
         Mockito.when(productRepository.save(product)).thenReturn(product);
@@ -40,7 +44,7 @@ class ProductServiceImplTest {
     }
 
     @Test
-    void deleteProduct() {
+    void ProductServiceCanDeleteAProduct() {
         Product product = new Product();
 
         Mockito.when(productRepository.findById(1L)).thenReturn(Optional.of(product));
@@ -52,7 +56,7 @@ class ProductServiceImplTest {
     }
 
     @Test
-    void deleteWhenProductNotExist() {
+    void ProductServiceCantDeleteWhenProductDoesNotExist() {
         Product product = new Product();
 
         Mockito.when(productRepository.findById(1L)).thenReturn(Optional.empty());
@@ -63,12 +67,30 @@ class ProductServiceImplTest {
         assertFalse(sut);
     }
 
-
     @Test
-    void getAllProducts (){
+    void ProductServiceCantGetAProductById() {
         Product product = new Product();
 
+        Mockito.when(productRepository.findById(1L)).thenReturn(Optional.of(product));
+        var productService = new ProductServiceImpl(productRepository, ownerRepository);
+        var sut = productService.getById(1L);
+
+        assertEquals(sut, product);
     }
+
+   /* @Test
+    void ProductServiceCantUpdateAProduct() {
+        Product product = new Product();
+        var newOwner = new Owner();
+
+        Mockito.when(productRepository.save(product)).thenReturn(product);
+        Mockito.when(ownerRepository.findById(1L)).thenReturn(Optional.of(newOwner));
+
+        var productService = new ProductServiceImpl(productRepository, ownerRepository);
+        var sut = productService.update(product, 1L);
+
+        assertEquals(sut, product);
+    }*/
 
 
 }
