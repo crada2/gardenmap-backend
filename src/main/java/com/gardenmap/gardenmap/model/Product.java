8cbb5fa3d4d5fe776e458d6c82a0bedb05fbfae9
Sol Turipe <solcis.bio@gmail.com>
2022-03-27 22:45:33 +0200

    I organized the code a bit

--- src/main/java/com/gardenmap/gardenmap/model/Product.java
+++ src/main/java/com/gardenmap/gardenmap/model/Product.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
-
 @Entity
 @Table (name="products")
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
--- src/main/java/com/gardenmap/gardenmap/service/UserServiceImpl.java
+++ src/main/java/com/gardenmap/gardenmap/service/UserServiceImpl.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Optional;
 
-
 @Service
 public class UserServiceImpl implements UserService {
 
